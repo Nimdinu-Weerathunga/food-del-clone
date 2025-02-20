999f9b46f458f0ca0e8425f8dedb785847cc1322
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-02-20 22:19:47 +0530

    Your commit message here

--- backendclone02/index.js
+++ backendclone02/index.js
@@ -10,3 +10,8 @@ app.use(bodyParser.json());
 
 //let mongoUrl = "mongodb+srv://Admin:123@cluster0.s3sie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 //mongoose.connect(mongoUrl)
+
+let connection = mongoose.connection
+connection.once("open",()=>{
+    console.log("MongoDB connection established successfully")
+})
