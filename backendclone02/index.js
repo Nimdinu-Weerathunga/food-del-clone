edb11323596cded05a1c9bdcd738a53ba7b37298
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-02-20 22:18:00 +0530

    Your commit message here

--- backendclone02/index.js
+++ backendclone02/index.js
@@ -7,3 +7,6 @@ import studentRouter from "./routes/studentRoute.js";
 let app = express();
 
 app.use(bodyParser.json());
+
+//let mongoUrl = "mongodb+srv://Admin:123@cluster0.s3sie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//mongoose.connect(mongoUrl)
