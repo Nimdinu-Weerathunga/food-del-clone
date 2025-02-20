5fd6a15e5e47ad1cf96662ce26b3417dc45c2ab3
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-02-20 22:16:59 +0530

    Your commit message here

--- backendclone02/index.js
+++ backendclone02/index.js
@@ -2,3 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import studentRouter from "./routes/studentRoute.js";
+
+
+let app = express();
+
+app.use(bodyParser.json());
